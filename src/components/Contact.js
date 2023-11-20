729a060c66dc93e298eb1f0565539f7600daa47f
Abhirama P <abhipatnadka33@gmail.com>
2023-11-20 13:19:55 +0530

    Added changes

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,25 +1,58 @@
 import "../css/Nav.css";
+import React, { useRef } from "react";
+import emailjs, { send } from "@emailjs/browser";
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_fgtgr7b",
+        "template_monk482",
+        form.current,
+        "kX7FiGDLE8zF4NyhS"
+      )
+      .then(
+        (result) => {
+          alert("Message sent");
+          form.current.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
   return (
     <>
       <div className="contact">
-    <div className="table_container">
-        <h1 className="con">Contact Me</h1>
-            <form action="">
-                <label>Name:</label>
-                <input type="text" id="name" placeholder="Please enter Your Name"/>
-        
-                
-                <label>Email:</label>
-                <input type="text" id="email" placeholder="Please enter Your email"/>
-            
-                   
+        <div className="table_container">
+          <h1 className="con">Contact Me</h1>
+          <form ref={form} onSubmit={sendEmail}>
+            <label>Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="Username"
+              placeholder="Please enter Your Name"/>
+            <label>Email:</label>
+            <input
+              type="text"
+              id="email"
+              name="Email"
+              placeholder="Please enter Your email"/>
+              <label>Suggestions:</label>
+            <input
+              type="text"
+              id="suggestion"
+              name="Suggestion"
+              placeholder="Any suggestions??"/>
             <div className="btn">
-                    <button type="submit">Reach out to me</button>
+              <button type="submit">Reach out to me</button>
             </div>
-            </form>
+          </form>
         </div>
-        </div>
+      </div>
     </>
   );
 }
